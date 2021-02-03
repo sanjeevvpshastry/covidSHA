fc9ca12cc5b5461b918ea15074490ea115f77b53
sanjeevvpshastry <77676048+sanjeevvpshastry@users.noreply.github.com>
2021-02-03 11:02:26 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { SecurePagesGuard } from './secure-pages.guard';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { CountByCountryComponent } from './count-by-country/count-by-country.component';
 import { SigninComponent } from './signin/signin.component';
 import { WorldcountComponent } from './worldcount/worldcount.component';
+import {WorldnewsComponent} from './worldnews/worldnews.component';
 
 const routes: Routes = [
   {path: "signin", component: SigninComponent,canActivate: [SecurePagesGuard]},
   {path:"worldcount", component: WorldcountComponent,canActivate:[AuthGuard]},
   {path: "count-by-country",children:[{path: "**", component: CountByCountryComponent, canActivate: [AuthGuard]}]},
+  {path: "worldnews",component: WorldnewsComponent},
   {path:"", pathMatch:"full", redirectTo:"signin"},
   {path:"**", redirectTo:"signin"}
 
